449a8de5d06b20127460414388e090a6d15478dd
udacchi <takashi.19740727@gmail.com>
2025-09-28 14:16:17 +0900

    ルーティング

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,18 +1,37 @@
 <?php
 
+// routes/web.php
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AttendanceController;
+use App\Http\Controllers\StampCorrectionRequestController;
+use App\Http\Controllers\Admin\AttendanceController as AdminAttendanceController;
+use App\Http\Controllers\Admin\StampCorrectionApprovalController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// 一般ユーザー
+Route::middleware(['auth', 'verified'])->group(function () {
+Route::get('/attendance/stamp', [AttendanceController::class, 'stamp'])->name('attendance.stamp');
+Route::get('/attendance', [AttendanceController::class, 'index'])->name('attendance.index');
 
-Route::get('/', function () {
-    return view('welcome');
+// ← モデルに合わせて {attendanceDay} に変更（Route Model Binding）
+Route::get('/attendance/{attendanceDay}', [AttendanceController::class, 'show'])->name('attendance.show');
+
+Route::get('/stamp-correction-requests', [StampCorrectionRequestController::class, 'index'])
+->name('stamp_correction_request.index');
+});
+
+// 管理者
+Route::prefix('admin')->name('admin.')->middleware(['auth', 'verified', 'can:admin'])->group(function () {
+Route::get('/attendance', [AdminAttendanceController::class, 'index'])->name('attendance.index');
+
+// ← バインディング名を {attendanceDay} に統一
+Route::get('/attendance/{attendanceDay}', [AdminAttendanceController::class, 'show'])->name('attendance.show');
+
+Route::get('/staff/{user}/attendance', [AdminAttendanceController::class, 'staffAttendance'])
+->name('staff.attendance.index');
+
+Route::get('/stamp-corrections', [StampCorrectionApprovalController::class, 'index'])->name('corrections.index');
+
+// ← {correctionRequest} に合わせる
+Route::get('/stamp-corrections/{correctionRequest}', [StampCorrectionApprovalController::class, 'show'])
+->name('corrections.show');
 });
