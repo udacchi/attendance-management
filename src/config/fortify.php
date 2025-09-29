514cefacbf414e7825118a6c385761858b968e44
udacchi <takashi.19740727@gmail.com>
2025-09-29 23:57:45 +0900

    ２FA削除

--- src/config/fortify.php
+++ src/config/fortify.php
@@ -148,14 +148,8 @@ return [
         Features::registration(),
         Features::resetPasswords(),
         Features::emailVerification(),
-        // Features::emailVerification(),
         Features::updateProfileInformation(),
         Features::updatePasswords(),
-        Features::twoFactorAuthentication([
-            'confirm' => true,
-            'confirmPassword' => true,
-            // 'window' => 0,
-        ]),
     ],
 
 ];
