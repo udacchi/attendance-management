51f3e3df9b4a69a4098fa0ac765a6f4676e7da96
udacchi <takashi.19740727@gmail.com>
2025-10-16 21:40:56 +0900

    ログイン画面修正

--- src/resources/views/admin/login.blade.php
+++ src/resources/views/admin/login.blade.php
@@ -17,17 +17,13 @@
       <div class="form-group">
         <label for="email" class="form-label">メールアドレス</label>
         <input id="email" name="email" type="email" class="form-input" value="{{ old('email') }}" autocomplete="email" autofocus required>
-        @error('email')
-          <p class="form-error">{{ $message }}</p>
-        @enderror
+        @error('email') <p class="form-error">{{ $message }}</p> @enderror
       </div>
 
       <div class="form-group">
         <label for="password" class="form-label">パスワード</label>
         <input id="password" name="password" type="password" class="form-input" autocomplete="current-password" required>
-        @error('password')
-          <p class="form-error">{{ $message }}</p>
-        @enderror
+        @error('password') <p class="form-error">{{ $message }}</p> @enderror
       </div>
 
       <button type="submit" class="admin-login__button">管理者ログインする</button>
