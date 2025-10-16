49a384f615a56041ae54a8a3d12c02adbb76c825
udacchi <takashi.19740727@gmail.com>
2025-10-16 21:41:33 +0900

    登録画面修正

--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -7,38 +7,34 @@
 @endsection
 
 @section('content')
-<div class="register-page">
-  <div class="register-panel">
-    <h1 class="register-title">会員登録</h1>
+<div class="user-register">
+  <div class="user-register__panel">
+    <h1 class="user-register__title">会員登録</h1>
 
-    <form class="register-form" action="{{ route('register') }}" method="POST" novalidate>
+    <form class="user-register__form" action="{{ route('register') }}" method="POST" novalidate>
       @csrf
 
       <div class="form-group">
         <label for="name" class="form-label">名前</label>
-        <input id="name" name="name" type="text" class="form-input"
-               value="{{ old('name') }}" autocomplete="name" autofocus required>
+        <input id="name" name="name" type="text" class="form-input" value="{{ old('name') }}" autocomplete="name" autofocus required>
         @error('name') <p class="form-error">{{ $message }}</p> @enderror
       </div>
 
       <div class="form-group">
         <label for="email" class="form-label">メールアドレス</label>
-        <input id="email" name="email" type="email" class="form-input"
-               value="{{ old('email') }}" autocomplete="email" required>
+        <input id="email" name="email" type="email" class="form-input" value="{{ old('email') }}" autocomplete="email" required>
         @error('email') <p class="form-error">{{ $message }}</p> @enderror
       </div>
 
       <div class="form-group">
         <label for="password" class="form-label">パスワード</label>
-        <input id="password" name="password" type="password" class="form-input"
-               autocomplete="new-password" required>
+        <input id="password" name="password" type="password" class="form-input" autocomplete="new-password" required>
         @error('password') <p class="form-error">{{ $message }}</p> @enderror
       </div>
 
       <div class="form-group">
         <label for="password_confirmation" class="form-label">パスワード確認</label>
-        <input id="password_confirmation" name="password_confirmation" type="password" class="form-input"
-               autocomplete="new-password" required>
+        <input id="password_confirmation" name="password_confirmation" type="password" class="form-input" autocomplete="new-password" required>
       </div>
 
       <button type="submit" class="register-button">登録する</button>
