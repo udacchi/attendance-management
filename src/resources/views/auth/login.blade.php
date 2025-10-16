48b17ee798e98d7423c3119cf1cc76c7b51bff62
udacchi <takashi.19740727@gmail.com>
2025-10-16 21:46:53 +0900

    ユーザーログイン画面修正

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -7,24 +7,22 @@
 @endsection
 
 @section('content')
-<div class="login-page">
-  <div class="login-panel">
-    <h1 class="login-title">ログイン</h1>
+<div class="user-login">
+  <div class="user-login__panel">
+    <h1 class="user-login__title">ログイン</h1>
 
-    <form class="login-form" action="{{ route('login') }}" method="POST" novalidate>
+    <form class="user-login__form" action="{{ route('login') }}" method="POST" novalidate>
       @csrf
 
       <div class="form-group">
         <label for="email" class="form-label">メールアドレス</label>
-        <input id="email" name="email" type="email" class="form-input"
-               value="{{ old('email') }}" autocomplete="email" autofocus required>
+        <input id="email" name="email" type="email" class="form-input" value="{{ old('email') }}" autocomplete="email" autofocus required>
         @error('email') <p class="form-error">{{ $message }}</p> @enderror
       </div>
 
       <div class="form-group">
         <label for="password" class="form-label">パスワード</label>
-        <input id="password" name="password" type="password" class="form-input"
-               autocomplete="current-password" required>
+        <input id="password" name="password" type="password" class="form-input" autocomplete="current-password" required>
         @error('password') <p class="form-error">{{ $message }}</p> @enderror
       </div>
 
