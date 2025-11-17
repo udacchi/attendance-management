7826fc543b0a5d02ad4e3d84466817caea7f763c
udacchi <takashi.19740727@gmail.com>
2025-11-17 23:40:51 +0900

    更新

--- src/resources/lang/ja/auth.php
+++ src/resources/lang/ja/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed'    => 'ログイン情報が登録されていません',
+    'password'  => 'ログイン情報が登録されていません',
+    'throttle'  => 'ログイン試行回数が多すぎます。:seconds秒後に再度お試しください',
 
 ];
