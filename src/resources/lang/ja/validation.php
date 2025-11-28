80d7d2da792f3c7e17695d10265300c2a2ae4a04
udacchi <takashi.19740727@gmail.com>
2025-11-29 01:24:35 +0900

    'confirmed'変更

--- src/resources/lang/ja/validation.php
+++ src/resources/lang/ja/validation.php
@@ -2,19 +2,19 @@
 
 return [
     // よく使うルールの日本語
-    'required' => ':attribute を入力してください。',
-    'email'    => ':attribute の形式が正しくありません。',
+    'required' => ':attribute を入力してください',
+    'email'    => ':attribute の形式が正しくありません',
     'min'      => [
-        'string' => ':attribute は :min 文字以上で入力してください。',
+        'string' => ':attribute は :min 文字以上で入力してください',
     ],
     'max'      => [
-        'string' => ':attribute は :max 文字以下で入力してください。',
+        'string' => ':attribute は :max 文字以下で入力してください',
     ],
-    'confirmed' => ':attribute が確認用と一致しません。',
-    'unique'    => ':attribute は既に使用されています。',
-    'string'    => ':attribute は文字列で入力してください。',
-    'numeric'   => ':attribute は数値で入力してください。',
-    'regex'     => ':attribute の形式が正しくありません。',
+    'confirmed' => ':attribute と一致しません',
+    'unique'    => ':attribute は既に使用されています',
+    'string'    => ':attribute は文字列で入力してください',
+    'numeric'   => ':attribute は数値で入力してください',
+    'regex'     => ':attribute の形式が正しくありません',
 
     // 属性名の日本語化（ここが最後に来る＝上書きされない）
     'attributes' => [
