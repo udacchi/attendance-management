1508fd7e3fee1564725633c2c51032ca59e53e42
udacchi <takashi.19740727@gmail.com>
2025-12-03 01:58:59 +0900

    余計なスペースの削除

--- src/resources/lang/ja/validation.php
+++ src/resources/lang/ja/validation.php
@@ -2,19 +2,19 @@
 
 return [
     // よく使うルールの日本語
-    'required' => ':attribute を入力してください',
-    'email'    => ':attribute の形式が正しくありません',
+    'required' => ':attributeを入力してください',
+    'email'    => ':attributeの形式が正しくありません',
     'min'      => [
-        'string' => ':attribute は :min 文字以上で入力してください',
+        'string' => ':attributeは:min文字以上で入力してください',
     ],
     'max'      => [
-        'string' => ':attribute は :max 文字以下で入力してください',
+        'string' => ':attributeは:max文字以下で入力してください',
     ],
-    'confirmed' => ':attribute と一致しません',
-    'unique'    => ':attribute は既に使用されています',
-    'string'    => ':attribute は文字列で入力してください',
-    'numeric'   => ':attribute は数値で入力してください',
-    'regex'     => ':attribute の形式が正しくありません',
+    'confirmed' => ':attributeと一致しません',
+    'unique'    => ':attributeは既に使用されています',
+    'string'    => ':attributeは文字列で入力してください',
+    'numeric'   => ':attributeは数値で入力してください',
+    'regex'     => ':attributeの形式が正しくありません',
 
     // 属性名の日本語化（ここが最後に来る＝上書きされない）
     'attributes' => [
